9d69681e34bbe8f2e1322d62d949b785136e9385
vamc97 <mallelavamshi2@gmail.com>
2023-08-25 15:17:11 -0500

    test

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-# resource "local_file" "pet" {
-#     filename = "pets.txt"
-#     content = "i love pets!"
+resource "local_file" "pet" {
+     filename = "pets.txt"
+     content = "i love pets!"
+ }
+
+ resource "local_file" "cat" {
+     filename = "cats.txt"
+     content = " My cat name is Mr.whiskers"
   
-# }
-# resource "local_file" "cat" {
-#     filename = "cats.txt"
-#     content = " My cat name is Mr.whiskers"
-  
-# }
+ }
